5784d4cd846bac8e6dcc2a6b40f7618793adf529
Grayroof <1grayroof@gmail.com>
2023-11-04 22:25:35 +0300

    test: create FlightFilterImplTest

---
+++ src/test/java/com/gridnine/testing/filterService/FlightFilterImplTest.java
@@ -0,0 +1,22 @@
+package com.gridnine.testing.filterService;
+
+import org.junit.jupiter.api.Test;
+
+class FlightFilterImplTest {
+
+    @Test
+    void shouldReturnFilteredResult() {
+    }
+
+    @Test
+    void departureBefore() {
+    }
+
+    @Test
+    void arrivalBeforeDeparture() {
+    }
+
+    @Test
+    void totalTransferAtLeast() {
+    }
+}
